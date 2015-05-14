0811cb72e08f97dc7159243abb935ba6de077ff0
Alexander Fomin <af@mindfor.ru>
2015-05-14 18:42:19 +0500

    Renamed root file and fixed tests
    Also updated fresh-css dependency and removed btn-mf because fresh has btn-link.

--- test/index.js
+++ test/index.js
@@ -2,21 +2,30 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 
 describe('#css', function () {
-	it('css-mf/_all.less', function (done) {
-		gulp.src('css-mf/_all.less')
-			.pipe(less())
+	it('pure.less', function (done) {
+		gulp.src('pure.less')
+			.pipe(less({
+				paths: [ "bower_components/pure-client" ]
+			}))
+			.pipe(gulp.dest("test/.temp"))
 			.on('end', done);
 	});
 
 	it('css-pages/admin.less', function (done) {
 		gulp.src('css-pages/admin.less')
-			.pipe(less())
+			.pipe(less({
+				paths: [ "bower_components/pure-client" ]
+			}))
+			.pipe(gulp.dest("test/.temp"))
 			.on('end', done);
 	});
 	
 	it('css-pages/focus.less', function (done) {
 		gulp.src('css-pages/focus.less')
-			.pipe(less())
+			.pipe(less({
+				paths: [ "bower_components/pure-client" ]
+			}))
+			.pipe(gulp.dest("test/.temp"))
 			.on('end', done);
 	});
 });
