3aec032614c339a857b4b99663b65f6cdb29b2be
Dmitry Braun <db@mindfor.ru>
2015-05-20 19:18:10 +0500

    Updated form button disabling

--- js/mf.forms.js
+++ js/mf.forms.js
@@ -1,10 +1,16 @@
 ﻿(function ($) {
 	$(function () {
 		//  disabled submit buttons on click and validate
-		$("form").submit(function () {
-			var $form = $(this);
-			if (!$form.valid || $form.valid())
-				$form.find("[type='submit']").prop("disabled", true);
+		$("form").submit(function (e) {
+			var form = this;
+			var $form = $(form);
+			if (!$form.valid || $form.valid()) {
+				e.preventDefault();
+				form.submit();
+				setTimeout(function () { // for Chrome
+					$form.find("[type='submit']").prop("disabled", true);
+				}, 500);
+			}
 		});
 	})
 })(jQuery);
