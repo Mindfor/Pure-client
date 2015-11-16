f6878ba721de849888baef09695b76d6c2d08d99
Niaro <niaronir@gmail.com>
2015-11-16 17:02:19 +0500

    Created mf.startup.js file with functions that need to be sometimes executed in the first place.

---
+++ js/mf.startup.js
@@ -0,0 +1,30 @@
+window.mf = window.mf || {};
+
+mf.parseFloat = function (value) {
+	if (typeof value == "string")
+		value = value.replace(/\s/g, "").replace(/,/g, ".");
+	return parseFloat(value);
+}
+
+mf.guid = (function () {
+	function s4() {
+		return Math.floor((1 + Math.random()) * 0x10000)
+				   .toString(16)
+				   .substring(1);
+	}
+	return function () {
+		return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+			   s4() + '-' + s4() + s4() + s4();
+	};
+})();
+
+mf.parseBool = function (value) {
+	if (value && typeof value == "string") {
+		switch (value.toLowerCase()) {
+			case "true": case "yes": case "1": return true;
+			case "false": case "no": case "0": case null: return false;
+			default: return Boolean(value);
+		}
+	}
+	return false;
+};
