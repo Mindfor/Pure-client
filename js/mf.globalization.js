9229ca84aa7d03e9a0c1ca5f74f147ca7821e15d
Niaro <niaronir@gmail.com>
2016-02-01 19:47:39 +0500

    Изменил пути в бандлах, для новой системы хранения либ) Поправил пути в `globalization.json.bundle`

--- js/mf.globalization.js
+++ js/mf.globalization.js
@@ -2,20 +2,19 @@
 window.mf = window.mf || {};
 (function () {
     $(function () {
-
         if (!Globalize)
             return;
 
         // Use $.getJSON instead of $.get if your server is not configured to return the
         // right MIME type for .json files.
         $.when(
-            $.get("/json/globalization/main/ru/ca-gregorian.json"),
-            $.get("/json/globalization/main/ru/numbers.json"),
-            $.get("/json/globalization/main/ru/timeZoneNames.json"),
-            $.get("/json/globalization/supplemental/likelySubtags.json"),
-            $.get("/json/globalization/supplemental/numberingSystems.json"),
-            $.get("/json/globalization/supplemental/timeData.json"),
-            $.get("/json/globalization/supplemental/weekData.json")
+            $.get("/globalization/ca-gregorian.json"),
+            $.get("/globalization/numbers.json"),
+            $.get("/globalization/timeZoneNames.json"),
+            $.get("/globalization/likelySubtags.json"),
+            $.get("/globalization/numberingSystems.json"),
+            $.get("/globalization/timeData.json"),
+            $.get("/globalization/weekData.json")
         ).then(function () {
             // Normalize $.get results, we only need the JSON, not the request statuses.
             return [].slice.apply(arguments, [0]).map(function (result) {
