01ea80e893a3b83191446cdbff218b8898f65d3d
Niaro <niaronir@gmail.com>
2016-02-12 17:23:29 +0500

    Вынес настройки для магнифик изображений в статрап - чтобы настройки были доступны всем клиентам

--- js/mf.pure.js
+++ js/mf.pure.js
@@ -147,48 +147,6 @@ window.mf = window.mf || {};
             });
 
 		// gallery
-		$.extend(true, $.magnificPopup.defaults, {
-			closeMarkup: "<button title='%title%' class='mfp-close'><i class='mfp-close-icn'>&times;</i></button>",
-			tClose: 'Закрыть (Esc)', // Alt text on close button
-			tLoading: 'Загрузка...', // Text that is displayed during loading. Can contain %curr% and %total% keys
-			gallery: {
-				arrowMarkup: "<button title='%title%' type='button' class='mfp-arrow mfp-arrow-%dir%'></button>", // markup of an arrow button
-				tPrev: "Предыдущее изображение (Клавиша стрелка влево)", // title for left button
-				tNext: "Следующее изображение (Клавиша стрелка вправо)", // title for right button
-				tCounter: "<span class='mfp-counter'>%curr% из %total%</span>" // markup of counter
-			},
-			image: {
-				tError: '<a href="%url%">Изображение</a> не удалось загрузить.' // Error message when image could not be loaded
-			},
-			ajax: {
-				tError: '<a href="%url%">Содержимое</a> не удалось загрузить.' // Error message when ajax request failed
-			}
-		});
-
-		mf.imageViewMagnificOptions = {
-			type: "image",
-			closeOnContentClick: true,
-			closeBtnInside: false,
-			mainClass: "mfp-with-zoom mfp-img-mobile",
-			image: {
-				horizontalFit: true
-			},
-			zoom: {
-				enabled: true,
-				duration: 200, //also don't foget to change the duration in CSS
-				opener: function(element) {
-					return element.find("img");
-				}
-			}
-		}
-
-		mf.galleryViewMagnificOptions = $.extend({}, mf.imageViewMagnificOptions, {
-			gallery: {
-				enabled: true
-			}
-		});
-
-		// gallery
 		$(".mf-gallery a").magnificPopup(mf.galleryViewMagnificOptions);
 		$("a.viewable").magnificPopup(mf.imageViewMagnificOptions);
         
--- js/mf.startup.js
+++ js/mf.startup.js
@@ -30,8 +30,49 @@ mf.parseBool = function (value) {
 };
 
 $(function () {
-	//current culture
-	mf.culture = $("html").attr("lang");
-	if (mf.culture === undefined)
-		mf.culture = "en";
+    //current culture
+    mf.culture = $("html").attr("lang");
+    if (mf.culture === undefined)
+        mf.culture = "en";
+
+    $.extend(true, $.magnificPopup.defaults, {
+        closeMarkup: "<button title='%title%' class='mfp-close'><i class='mfp-close-icn'>&times;</i></button>",
+        tClose: 'Закрыть (Esc)', // Alt text on close button
+        tLoading: 'Загрузка...', // Text that is displayed during loading. Can contain %curr% and %total% keys
+        gallery: {
+            arrowMarkup: "<button title='%title%' type='button' class='mfp-arrow mfp-arrow-%dir%'></button>", // markup of an arrow button
+            tPrev: "Предыдущее изображение (Клавиша стрелка влево)", // title for left button
+            tNext: "Следующее изображение (Клавиша стрелка вправо)", // title for right button
+            tCounter: "<span class='mfp-counter'>%curr% из %total%</span>" // markup of counter
+        },
+        image: {
+            tError: '<a href="%url%">Изображение</a> не удалось загрузить.' // Error message when image could not be loaded
+        },
+        ajax: {
+            tError: '<a href="%url%">Содержимое</a> не удалось загрузить.' // Error message when ajax request failed
+        }
+    });
+
+    mf.imageViewMagnificOptions = {
+        type: "image",
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        mainClass: "mfp-with-zoom mfp-img-mobile",
+        image: {
+            horizontalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 200, //also don't foget to change the duration in CSS
+            opener: function (element) {
+                return element.find("img");
+            }
+        }
+    }
+
+    mf.galleryViewMagnificOptions = $.extend({}, mf.imageViewMagnificOptions, {
+        gallery: {
+            enabled: true
+        }
+    });
 });
